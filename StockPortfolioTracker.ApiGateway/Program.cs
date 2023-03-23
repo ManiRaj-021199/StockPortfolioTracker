2f1537bdc97a0b6326da4a0053052d7a22e4d0e8
Mani Raj <maniraj.021199@gmail.com>
2023-03-23 19:03:48 +0530

    CHANGED: Project Structure Updated.

---
+++ StockPortfolioTracker.ApiGateway/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StockPortfolioTracker.ApiGateway
+{
+    public class Program
+    {
+        #region Publics
+        public static void Main(string[] args)
+        {
+            Startup.BuildWebHost(args).Run();
+        }
+        #endregion
+    }
+}
---
+++ StockPortfolioTracker.ApiGateway/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace StockPortfolioTracker.ApiGateway
+{
+    public class Startup
+    {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IHostEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(hostEnvironment.ContentRootPath)
+                   .AddJsonFile(Path.Combine("Configuration", "configuration.json"), optional: false, reloadOnChange: true)
+                   .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args);
+
+            builder.ConfigureServices(s => s.AddSingleton(builder))
+                   .ConfigureAppConfiguration(ic => ic.AddJsonFile(Path.Combine("Configuration", "configuration.json")))
+                   .UseStartup<Startup>();
+            var host = builder.Build();
+            return host;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot(Configuration);
+        }
+
+        public async void Configure(IApplicationBuilder app)
+        {
+            await app.UseOcelot();
+        }
+    }
+}
