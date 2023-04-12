101fd0ecd04624e34e3c87a662de13044e02d092
Mani Raj <maniraj.021199@gmail.com>
2023-04-12 16:24:11 +0530

    ADDED: Page Template & Db Connection added.

---
+++ StockPortfolioTracker.Web/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace StockPortfolioTracker.Web;
+
+public class Startup
+{
+    #region Publics
+    public static WebApplication InitializeApp(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        ConfigureServices(builder);
+
+        WebApplication app = builder.Build();
+        Configure(app);
+
+        return app;
+    }
+    #endregion
+
+    #region Privates
+    private static void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+    }
+
+    private static void Configure(WebApplication app)
+    {
+        if(!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+    }
+    #endregion
+}
