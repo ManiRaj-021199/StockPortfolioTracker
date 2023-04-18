6e4442a9a995d38c4286a79f9199b61e39ec931a
Mani Raj <maniraj.021199@gmail.com>
2023-04-18 17:09:40 +0530

    ADDED: Login Logout Functionality Added (without db).

--- StockPortfolioTracker.Web/Pages/Authentication/Login/LoginBase.cs
+++ StockPortfolioTracker.Web/Pages/Authentication/Login/LoginBase.cs
@@ -1,7 +1,40 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using StockPortfolioTracker.Common;
 
 namespace StockPortfolioTracker.Web;
 
 public class LoginBase : ComponentBase
 {
+    #region Properties
+    [Inject]
+    public AuthenticationStateProvider? AuthenticationStateProvider { get; set; }
+
+    [Inject]
+    public ISessionStorageService? SessionStorageService { get; set; }
+
+    [Inject]
+    public NavigationManager? NavigationManager { get; set; }
+
+    protected User? User { get; set; }
+    #endregion
+
+    #region Protecteds
+    protected override Task OnInitializedAsync()
+    {
+        this.User = new User();
+        return base.OnInitializedAsync();
+    }
+
+    protected async Task<bool> ValidateUser()
+    {
+        ((CustomAuthenticationStateProvider) this.AuthenticationStateProvider!).MarkUserAsAuthenticated(this.User?.Email!);
+        this.NavigationManager?.NavigateTo("/");
+
+        await this.SessionStorageService!.SetItemAsync("user-email", this.User!.Email);
+
+        return await Task.FromResult(true);
+    }
+    #endregion
 }
