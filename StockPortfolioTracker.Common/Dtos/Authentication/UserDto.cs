9d4882a5fc7f94e0221e91f258385b9c073dc44f
Mani Raj <maniraj.021199@gmail.com>
2023-04-20 16:59:03 +0530

    MODIFIED: Add Properties to UserDto.

--- StockPortfolioTracker.Common/Dtos/Authentication/UserDto.cs
+++ StockPortfolioTracker.Common/Dtos/Authentication/UserDto.cs
@@ -8,5 +8,7 @@ public class UserDto
     public string? Email { get; set; }
     public string? Password { get; set; }
     public string? ConfirmPassword { get; set; }
+    public string? PasswordHash { get; set; }
+    public byte[]? PasswordSalt { get; set; }
     #endregion
 }
