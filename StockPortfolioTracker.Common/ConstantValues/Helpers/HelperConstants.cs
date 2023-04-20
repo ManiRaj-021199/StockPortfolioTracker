3d5f89637d6b96e02bed4e8d3b70558d71a498bf
Mani Raj <maniraj.021199@gmail.com>
2023-04-20 18:57:32 +0530

    ADDED: JwtToken Added in Login Response.

--- StockPortfolioTracker.Common/ConstantValues/Helpers/HelperConstants.cs
+++ StockPortfolioTracker.Common/ConstantValues/Helpers/HelperConstants.cs
@@ -9,5 +9,8 @@ public static class HelperConstants
     public static readonly HashAlgorithmName HashAlgorithm = HashAlgorithmName.SHA512;
     public static readonly int KeySize = 64;
     public static readonly int Iterations = 350000;
+
+    // JWT Token
+    public static readonly string Token = "this is my custom Secret key for authentication";
     #endregion
 }
---
+++ StockPortfolioTracker.Logic/Helpers/AuthenticationHelper/JwtTokenHelper/JwtTokenHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using StockPortfolioTracker.Common;
+
+namespace StockPortfolioTracker.Logic;
+
+public class JwtTokenHelper
+{
+    #region Publics
+    public static string GenerateJwtToken(UserDto userDto)
+    {
+        List<Claim> claims = new()
+                             {
+                                 new Claim(ClaimTypes.Name, $"{userDto.FirstName} {userDto.LastName}"),
+                                 new Claim(ClaimTypes.Email, userDto.Email!)
+                             };
+
+        SymmetricSecurityKey secretKey = new(Encoding.UTF8.GetBytes(HelperConstants.Token));
+        SigningCredentials credentials = new(secretKey, SecurityAlgorithms.HmacSha512Signature);
+
+        JwtSecurityToken jwtSecurityToken = new(
+                                                claims: claims,
+                                                expires: DateTime.Now.AddDays(1),
+                                                signingCredentials: credentials);
+        string? strJwtToken = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+        return strJwtToken;
+    }
+    #endregion
+}
