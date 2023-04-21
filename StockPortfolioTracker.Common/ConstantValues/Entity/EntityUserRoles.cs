1c5127cd80ce9d7e0ba0fd758ff006f6b01b4a35
Mani Raj <maniraj.021199@gmail.com>
2023-04-21 19:10:57 +0530

    ADDED: Default User Role Added.

--- StockPortfolioTracker.Common/ConstantValues/Entity/EntityUserRoles.cs
+++ StockPortfolioTracker.Common/ConstantValues/Entity/EntityUserRoles.cs
@@ -3,6 +3,9 @@
 public class EntityUserRoles
 {
     #region Constants
+    public const int SUPERUSERID = 1;
+    public const int USERID = 2;
+
     public const string SUPERUSER = "Super User";
     public const string USER = "User";
     #endregion
