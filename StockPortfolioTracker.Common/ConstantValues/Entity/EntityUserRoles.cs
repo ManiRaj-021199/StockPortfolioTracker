09def3d8b0020a155c0f13144757464797f3b98a
Mani Raj <maniraj.021199@gmail.com>
2023-04-24 12:56:53 +0530

    ADDED: Authorization for StockStatistics MicroService.

--- StockPortfolioTracker.Common/ConstantValues/Entity/EntityUserRoles.cs
+++ StockPortfolioTracker.Common/ConstantValues/Entity/EntityUserRoles.cs
@@ -8,5 +8,8 @@ public class EntityUserRoles
 
     public const string SUPERUSER = "Super User";
     public const string USER = "User";
+
+    // Combination - For Authorization
+    public const string SUPERUSER_WITH_USER = $"{SUPERUSER},{USER}";
     #endregion
 }
