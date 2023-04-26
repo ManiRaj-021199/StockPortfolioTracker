004daf6570ce19ba77db769a4cd3bf2d8b0ed24f
Ajith Kumar <94164200+AntonyAjith@users.noreply.github.com>
2023-04-26 19:01:12 +0530

    MERGED: (PR - #12) Added Watchlist Project
    Co-authored-by: Ajithkumar <ajithrithvick26@gmail.com>

---
+++ MicroServices/Watchlist/Controllers/WatchlistController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Watchlist.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WatchlistController : ControllerBase
+{
+    #region Publics
+    [HttpGet]
+    [Route("GetAllWatchlistForTheUser")]
+    public string GetAllWatchlistForTheUser()
+    {
+        return "GetAllWatchlistForTheUser";
+    }
+    #endregion
+}
