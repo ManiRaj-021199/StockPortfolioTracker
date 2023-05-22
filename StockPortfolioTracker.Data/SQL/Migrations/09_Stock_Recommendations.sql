4313b6a7c247ff8f70a9a33c7198e5c7c795db73
Anbu <42139448+anbuanbalagan@users.noreply.github.com>
2023-05-22 16:49:12 +0530

    MERGED: (PR - 23) Stock Recommendation Scripts Added

---
+++ StockPortfolioTracker.Data/SQL/Migrations/09_Stock_Recommendations.sql
@@ -0,0 +1,15 @@
+﻿/****** Object:  Table [Stock].[Recommendations] Script Date: 22-05-2023 11:01:45 ******/
+IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[Stock].[Recommendations]') AND type in (N'U'))
+
+BEGIN
+CREATE TABLE [Stock].[Recommendations](
+	[RecommendationId] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[BranchId] [int] NOT NULL,
+	[Symbol] [nvarchar](50) NOT NULL,
+	[RecommendedBuyPrice] [int] NOT NULL,
+	[TargetPrice] [int] NOT NULL,
+	[RegisterDate] [datetime2](7) NOT NULL,
+	FOREIGN KEY (BranchId) REFERENCES [Organization].[Branches] (BranchId)
+)
+END
