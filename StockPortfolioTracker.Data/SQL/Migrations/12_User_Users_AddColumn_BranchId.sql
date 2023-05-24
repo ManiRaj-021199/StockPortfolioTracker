e1a327791323902fd90cfd6201288db33cca1d3f
Anbarasan <a.arunanbu25@gmail.com>
2023-05-24 17:12:42 +0530

    CHANGED: BranchId Added To Users Table

---
+++ StockPortfolioTracker.Data/SQL/Migrations/12_User_Users_AddColumn_BranchId.sql
@@ -0,0 +1,6 @@
+﻿/****** Object:  Table [User].Users    Script Date: 24-05-2023 10:33:59 ******/
+
+ALTER TABLE [User].Users 
+ADD [BranchId] [int] NOT NULL 
+FOREIGN KEY(BranchId) REFERENCES [User].Branches(BranchId)
+CONSTRAINT DF_Users_BranchId DEFAULT 1
